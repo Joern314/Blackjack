9927747c8808c469022ad6eade07a64133b0e0b8
joern <joern@Matrioshka>
2018-09-26 21:07:35 +0200

    working color sort of working dropdown menu (incomplete)

--- blackjack/addons/namecolor.js
+++ blackjack/addons/namecolor.js
@@ -1,22 +1,32 @@
+/* global sendPart, options */
+
 window.addEventListener("load", OnInit_namecolor, false);
+
+let usernames = [];
 
 function OnInit_namecolor() {
     console.log("Starting Plugin: namecolor.js");
-
-    let namePart = document.getElementById("name");
-    namePart.oninput = OnNameInput;
-
+    ModifyNameInput();
     OnNameInput();
+
+    addUsername("Lukas");
+    rebuildUsernamesDatalist();
+
+    console.log("done starting plugin");
 }
 
-function OnNameInput() {
+function addUsername(name) {
+    let color = calcColor(name);
+    usernames.push({name: name, color: color});
+}
+
+function recolorName() {
     let namePart = sendPart.getElementById("name");
     let name = namePart.value;
-    let colorHash = calcColor(name);
-    let col = PostColor({'color': colorHash});
-    let colorHex = col.toString(16);
+    let colorHex = calcColor(name);
+    let color = PostColor({'color': colorHex});
 
-    namePart.style.color = "#" + colorHex;
+    namePart.style.color = "#" + color;
 }
 
 function calcColor(name) {
@@ -38,6 +48,92 @@ function calcColor(name) {
 
     let color = (comp('a') << 16) | (comp('b') << 8) | (comp('c'));
 
-    calcColor.map[name] = color;
-    return color;
+    calcColor.map[name] = color.toString(16);
+
+    return calcColor.map[name];
+}
+
+function ModifyNameInput() {
+    const innerHTML = `
+        <label id="nameLabel" for="name">Name:</label>
+    
+        <div class="dropdown-div">
+            <input id="name" placeholder="Name" tabindex="1">
+            <div class="dropdown-content" id="usernamesContent">
+                <label>Lukas</label>
+                <label>Luise</label>
+            </div>
+        </div>
+     `;
+    //        <input id="nameButton" value="Select" onclick="OnNameButtonClick()">
+
+    // <p>
+    let ppart = document.getElementById("name").parentNode;
+    // replacement <div>
+    let elem = document.createElement('div');
+    elem.className = "dropdown-line";
+    elem.innerHTML = innerHTML;
+    
+    ppart.parentNode.replaceChild(elem, ppart);
+    
+    addCallbacks();
+}
+
+function addCallbacks() {
+    let name = document.getElementById("name");
+    name.onchange = OnNameChange;
+    name.oninput = OnNameInput;
+    name.addEventListener("keypress", function (event) {
+        if (event.keyCode === 38 || event.keyCode === 40) {
+            ToggleDropdown();
+        }
+    }, false);
+}
+
+function OnNameChange() {
+//    rebuildUsernamesDatalist();
+    UpdateSettings();
+}
+
+function OnNameInput() {
+    recolorName();
+}
+
+function ToggleDropdown() {
+    let udc =
+            document.getElementById("usernamesContent");
+    if (udc.style.display === "none") {
+        udc.style.display = "block";
+    } else {
+        udc.style.display = "none";
+    }
+}
+
+function OnNameButtonClick() {
+    console.log("event");
+
+    let nameButton = document.getElementById("nameButton");
+//    nameOptions.slideToggle(100);
+}
+
+function rebuildUsernamesDatalist() {
+    console.log(usernames);
+
+    let elem = document.getElementById("usernameList");
+    if (elem === null) {
+        return;
+    }
+    // clear
+    for (let i = elem.options.length - 1; i >= 0; i--)
+    {
+        elem.remove(i);
+    }
+    // rebuild
+    for (let i = usernames.length - 1; i >= 0; i--) {
+        let name = usernames[i];
+        let option = document.createElement('option');
+        option.value = name['name'];
+
+        elem.appendChild(option);
+    }
 }
--- blackjack/main.js
+++ blackjack/main.js
@@ -2,26 +2,104 @@
 
 
 // remove old chat.js from the headers
-console.log("Removing old chat.js");
+/*
+let selector = 'script[src="chat.js"]';
 
-let oldChatNode = document.querySelector('script[src="chat.js"]');
-oldChatNode.remove();
+let observer = new MutationObserver((mutations) => mutations.forEach((mutation) => {
+    let nodes = Array.from(mutation.addedNodes);
+    for(let node of nodes) {
+        console.log("node");
+        if(node.matches && node.matches(selector)) {
+            observer.disconnect();
+            deleteOldChatNode(node);
+            return;
+        }
+    }
+}));
+observer.observe(document.documentElement, {childList: true, subtree: true});
 
-console.log(oldChatNode);
+function deleteOldChatNode(node) {
+    console.log("Removing old chat.js");
+    node.remove();
+    console.log(node);
+}
 
-// inject our javascript addons
-injectLocalScript("addons/namecolor.js");
+*/
 
-// inject our javascript libraries
-injectLocalScript("js/md5.min.js");
+/*
+// redirect "chat.js"
+let pattern = "https://chat.qed-verein.de/*";
+function redirect(requestDetails) {
+    console.log("chat.js was requested");
+    return {
+        cancel: true
+    };
+}
+browser.webRequest.onBeforeRequest.addListener(
+   redirect,
+   {urls:[pattern], types:["main_frame"]},
+   ["blocking"]
+);
+*/
 
-function injectLocalScript(file) {
-    console.log("Injecting script: "+file);
+// REPLACE WITH MODIFIED index.html
+/*
+let localIndexHtml = browser.runtime.getURL("modified/index.html");
+const req = new XMLHttpRequest();
+
+req.onreadystatechange = function(event) {
+    if(this.readystate === XMLHttpRequest.DONE) {
+        if(this.status === 200) {
+            console.log("received modified index.html");
+        } else {
+            console.log("error: %d (%s)", this.status, this.statusText);
+        }
+    }
+}
+req.open('GET', localIndexHtml, true);
+req.send(null);
+
+document.open();
+document.write();
+document
+*/
+
+// DETECT TYPE OF WEBSITE
+// TODO 
+
+// INJECT NEW STUFF
+
+injectAddonsAndLibraries();
+
+function injectAddonsAndLibraries() {
+    // overwrite some chat functions
     
+    // inject our javascript addons
+    injectLocalScript("addons/namecolor.js");
+
+    // inject our new stylesheets
+    injectLocalCSS("css/namecolor.css");
+
+    // inject our javascript libraries
+    injectLocalScript("js/md5.min.js");
+}
+    
+function injectLocalScript(file) {
+    console.log("Injecting script: " + file);
+
     let elem = document.createElement('script');
     elem.src = browser.runtime.getURL(file);
     elem.type = 'text/javascript';
     document.head.appendChild(elem);
+}
+
+function injectLocalCSS(file) {
+    //<link rel="stylesheet" type="text/css" href="common.css">
+    console.log("Injecting css: "+file);
     
-    console.log("done");
+    let elem = document.createElement('link');
+    elem.rel = "stylesheet";
+    elem.type = "text/css";
+    elem.href = browser.runtime.getURL(file);
+    document.head.appendChild(elem);
 }
