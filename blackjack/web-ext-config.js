cd2735bcd1be1e681fcefbd8429c314ec07c8511
joern <joern@Matrioshka>
2018-12-07 22:07:48 +0100

    added web-ext support for signing and updates

---
+++ blackjack/web-ext-config.js
@@ -0,0 +1,13 @@
+module.exports = {
+    ignoreFiles: [
+        'official/*',
+        'updates.json',
+        'todo'
+    ],
+    // Global options:
+    verbose: true,
+    // Command options:
+    build: {
+        overwriteDest: true
+    }
+};
